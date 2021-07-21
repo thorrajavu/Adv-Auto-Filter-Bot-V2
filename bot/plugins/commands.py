437fd7a32d49a828d733b7adc8aded36ac9e2fe1
thorrajavu <81627740+thorrajavu@users.noreply.github.com>
2021-07-21 18:24:55 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Share and support', url="https://t.me/Malayalamnewdvd"
                                 )
                         ]
                     ]
